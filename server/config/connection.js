2d04f80e7cc174033e112793052082be92f61990
krish0423 <22it138@charusat.edu.in>
2024-04-05 10:26:01 +0530

    .xy

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://Krish:Krish%4004@cluster1.rv20t2c.mongodb.net/fitness-tracker", {
+
+mongoose.connect( "mongodb+srv://Krish:Krish@cluster1.rv20t2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }, err => {
--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const routes = require("./routes");
 const db = require("./config/connection");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,6 +21,16 @@ if (process.env.NODE_ENV === "production") {
 // });
 
 app.use(routes);
+
+// try {
+//   db.once("open", () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//     });
+//   });
+// } catch (error) {
+//   console.log(error);
+// }
 
 db.once("open", () => {
   app.listen(PORT, () => {
